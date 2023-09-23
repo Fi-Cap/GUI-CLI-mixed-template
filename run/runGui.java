edec5f0d75532d07059577069f4291e5bb0d6a0d
Fi-Cap <85383882+Fi-Cap@users.noreply.github.com>
2023-09-23 18:29:15 +0800

    Update runGui.java

--- run/runGui.java
+++ run/runGui.java
@@ -3,8 +3,13 @@ package com.org.example.run;
 import com.org.example.func.printEx;
 
 public class runGui implements Runnable{
+    public runGui() {
+		// put your code here to init, such as loading config.
+	}
+    
     @Override
     public void run() {
         printEx.debug("Run as GUI");
+        // put your code here, such as show the window, add comps to window.
     }
 }
