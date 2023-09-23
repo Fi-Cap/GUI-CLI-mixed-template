cee3a23f4a7c25b4b19a13ddd72c7347c4828089
Fi-Cap <85383882+Fi-Cap@users.noreply.github.com>
2023-09-23 18:50:43 +0800

    Update AppInfo.java

--- AppInfo.java
+++ AppInfo.java
@@ -1,5 +1,7 @@
 package com.org.example;
 
 public class AppInfo {
+    public final static String Name = "example";
     public static boolean Debug = false;
+    // put more info of the app here.
 }
