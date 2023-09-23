a0bc01860fb704e6a35773e4c08226c4b0433019
Fi-Cap <85383882+Fi-Cap@users.noreply.github.com>
2023-09-23 18:31:23 +0800

    Update runNogui.java

--- run/runNogui.java
+++ run/runNogui.java
@@ -3,8 +3,13 @@ package com.org.example.run;
 import com.org.example.func.*;
 
 public class runNogui implements Runnable{
+    public runNogui() {
+		// put your code here to init, such as loading config.
+	}
+    
     @Override
     public void run() {
         printEx.debug("Run as nogui");
+        // put your code here, such as process the input command.
     }
 }
